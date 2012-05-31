06fb7119711bbe198cc2eae00f63de6fd376d3ad
unknown <v.talas@talas011.cz.gmc.net>
2012-05-31 10:55:00 +0200

    less kompilace jede, rozbita cassette

--- cms/CassetteConfiguration.cs
+++ cms/CassetteConfiguration.cs
@@ -1,56 +1,56 @@
-using Cassette;
-using Cassette.Configuration;
-using Cassette.HtmlTemplates;
-using Cassette.Scripts;
-using Cassette.Stylesheets;
+//using Cassette;
+//using Cassette.Configuration;
+//using Cassette.HtmlTemplates;
+//using Cassette.Scripts;
+//using Cassette.Stylesheets;
 
-namespace cms
-{
-    /// <summary>
-    /// Configures the Cassette asset modules for the web application.
-    /// </summary>
-    public class CassetteConfiguration : ICassetteConfiguration
-    {
+//namespace cms
+//{
+//    /// <summary>
+//    /// Configures the Cassette asset modules for the web application.
+//    /// </summary>
+//    public class CassetteConfiguration : ICassetteConfiguration
+//    {
 
-        public void Configure(BundleCollection bundles, CassetteSettings settings)
-        {
+//        public void Configure(BundleCollection bundles, CassetteSettings settings)
+//        {
             
-			// TODO: Configure your bundles here...
-            // Please read http://getcassette.net/documentation/configuration
+//            // TODO: Configure your bundles here...
+//            // Please read http://getcassette.net/documentation/configuration
 
-            // This default configuration treats each file as a separate 'bundle'.
-            // In production the content will be minified, but the files are not combined.
-            // So you probably want to tweak these defaults!
-            bundles.AddPerIndividualFile<StylesheetBundle>("Content");
-            bundles.AddPerIndividualFile<ScriptBundle>("Scripts",null,
-				b => b.Processor = new ScriptPipeline
-				{
-					CoffeeScriptCompiler = new IECoffeeScriptCompiler()
-				});
+//            // This default configuration treats each file as a separate 'bundle'.
+//            // In production the content will be minified, but the files are not combined.
+//            // So you probably want to tweak these defaults!
+//            bundles.AddPerIndividualFile<StylesheetBundle>("Content");
+//            bundles.AddPerIndividualFile<ScriptBundle>("Scripts",null,
+//                b => b.Processor = new ScriptPipeline
+//                {
+//                    CoffeeScriptCompiler = new IECoffeeScriptCompiler()
+//                });
 
-            // To combine files, try something like this instead:
-            //   bundles.Add<StylesheetBundle>("Content");
-            // In production mode, all of ~/Content will be combined into a single bundle.
+//            // To combine files, try something like this instead:
+//            //   bundles.Add<StylesheetBundle>("Content");
+//            // In production mode, all of ~/Content will be combined into a single bundle.
 
-			bundles.Add<ScriptBundle>("Scripts/Admin",
-				b => b.Processor = new ScriptPipeline
-				{
-					CoffeeScriptCompiler = new IECoffeeScriptCompiler()
-				});
-
-
-			//Jquery HTml 
-			bundles.Add<HtmlTemplateBundle>("HtmlTemplates",
-				bundle => bundle.Processor = new JQueryTmplPipeline()
-			);
+//            bundles.Add<ScriptBundle>("Scripts/Admin",
+//                b => b.Processor = new ScriptPipeline
+//                {
+//                    CoffeeScriptCompiler = new IECoffeeScriptCompiler()
+//                });
 
 
+//            //Jquery HTml 
+//            bundles.Add<HtmlTemplateBundle>("HtmlTemplates",
+//                bundle => bundle.Processor = new JQueryTmplPipeline()
+//            );
 
-            // If you want a bundle per folder, try this:
-            //bundles.AddPerSubDirectory<ScriptBundle>("Scripts/Admin");
-            // Each immediate sub-directory of ~/Scripts will be combined into its own bundle.
-            // This is useful when there are lots of scripts for different areas of the website.
 
-        }
-    }
-}
+
+//            // If you want a bundle per folder, try this:
+//            //bundles.AddPerSubDirectory<ScriptBundle>("Scripts/Admin");
+//            // Each immediate sub-directory of ~/Scripts will be combined into its own bundle.
+//            // This is useful when there are lots of scripts for different areas of the website.
+
+//        }
+//    }
+//}
--- cms/Controllers/BootstrapController.cs
+++ cms/Controllers/BootstrapController.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Newtonsoft.Json.Linq;
 using cms.Code.Bootstraper;
 using dotless.Core;
+using dotless.Core.Importers;
+using dotless.Core.Input;
 using dotless.Core.Loggers;
 using dotless.Core.Parser;
 
@@ -20,20 +20,26 @@ namespace cms.Controllers
         }
         public ActionResult css()
         {
+			Directory.SetCurrentDirectory(Server.MapPath("~/App_Data/bootstrap/less/"));
+
 			var test = Server.MapPath("~/App_Data/bootstrap/less/bootstrap.less");
 
+			var parser = new Parser();
+        	var imp = new Importer(new FileReader(new ServerPathResolver(Server)));
+
+        	parser.Importer = imp;
 
 			var a = new LessEngine
 						{
-							Logger = new ResponseLogger(Response)
+							Logger = new ResponseLogger(Response),
+							Parser = parser
 						};
 
-
-			var css = a.TransformToCss(FileExts.GetContent(test), null);
-
+			//a.Compress = true;
+			var css = a.TransformToCss(FileExts.GetContent(test), test);
 			Response.ContentType = "text/css";
 			Response.Write(css);
-
+			Response.Write(Session.SessionID);
 
         	return new EmptyResult();
         }
@@ -55,6 +61,20 @@ namespace cms.Controllers
 
 		}
     }
+
+	public class ServerPathResolver : IPathResolver
+	{
+		private HttpServerUtilityBase Server { get; set; }
+		public ServerPathResolver(HttpServerUtilityBase server)
+		{
+			Server = server;
+		}
+
+		public string GetFullPath(string path)
+		{
+			return Server.MapPath(path);
+		}
+	}
 
 	public class ResponseLogger : ILogger
 	{
