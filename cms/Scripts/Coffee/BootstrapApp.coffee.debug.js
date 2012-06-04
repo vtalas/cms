4fd2a9af9bd99e4a5b853532ed196ce36596a294
ladesopeklo <vladimir.talas@gmail.com>
2012-06-04 22:28:50 +0200

    views

---
+++ cms/Scripts/Coffee/BootstrapApp.coffee.debug.js
@@ -0,0 +1,22 @@
+﻿(function() {
+  /*
+  @reference ../jquery-1.7.2.js
+  @reference ../angular.js
+  */
+  var aaaController, module;
+  module = angular.module("bootstrapApp", []);
+  module.config([
+    "$routeProvider", "$provide", function($routeProvider, $provide) {
+      return $routeProvider.when("/aaa", {
+        controller: aaaController,
+        template: "/Content/aaa.html"
+      }).when("/new", {
+        controller: aaaController,
+        template: "template/new"
+      });
+    }
+  ]);
+  aaaController = function($scope, $http) {
+    return 1;
+  };
+}).call(this);
