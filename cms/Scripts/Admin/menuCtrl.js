b316be59fb8810ee46c9ff0dbbcc83f4034d8cba
ladesopeklo <vladimir.talas@gmail.com>
2012-08-25 16:35:43 +0200

    menu

--- cms/Scripts/Admin/menuCtrl.js
+++ cms/Scripts/Admin/menuCtrl.js
@@ -1,8 +1,29 @@
-var menuCtrl = ['$scope', '$http', '$rootScope', 'appSettings', 'apimenu', function ($scope, $http, $rootScope, appSettings, apimenu) {
+var menusCtrl = ['$scope', '$http', '$rootScope', 'appSettings', 'apimenu', function($scope, $http, $rootScope, appSettings, apimenu) {
 
-	$scope.$on("setCultureEvent", function () {
+	$scope.$on("setCultureEvent", function() {
 		console.log("menuCtrl set culture")
-	})
+	});
+
+
+	$scope.data = apimenu.list({ }, function(d) {
+		console.log(d);
+	});
+
+	$scope.addclick = function() {
+		$scope.createNew = true;
+	};
+	$scope.createNewCancel = function() {
+		$scope.createNew = false;
+	};
+
+
+}];
+var menuCtrl = function ($scope, $http, $rootScope, appSettings, apimenu) {
+	menuCtrl.$inject = ["$scope", "$http", "apimenu", "appSettings"]
+
+	$scope.$on("setCultureEvent", function() {
+		console.log("menuCtrl set culture")
+	});
 
 
 	$scope.data = apimenu.list({}, function (d) {
@@ -17,4 +38,4 @@ var menuCtrl = ['$scope', '$http', '$rootScope', 'appSettings', 'apimenu', funct
 	};
 
 
-} ]
+} 
