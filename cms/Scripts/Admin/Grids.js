74d1c33eecd26aecda654760723f90901e7e7b7d
vladimir talas <vladimir.talas@gmail.com>
2012-09-06 15:42:27 +0200

    dnd namespace

--- cms/Scripts/Admin/Grids.js
+++ cms/Scripts/Admin/Grids.js
@@ -5,37 +5,54 @@ module.value('ui.config', {
 		allowClear: true
 	},
 	dropablehtml: {
-		ngdrop: function (e, uioptions, scope, placeholderitem) {
-			var collection,
-			    item,
-			    index;
+		pageslist: {
+			ngdrop: function (e, uioptions, scope, placeholderitem) {
+				var collection,
+					item,
+					index;
 
-			collection = scope.$parent.$collection;
-			item = scope.$root.draggeditem;
-			index = collection.indexOf(placeholderitem);
+				collection = scope.$parent.$collection;
+				item = scope.$root.draggeditem;
+				index = collection.indexOf(placeholderitem);
 
-			if (placeholderitem === null || uioptions.last) {
-				collection.push(item);
-			} else {
-				collection.splice(index, 0, item);
+				if (placeholderitem === null || uioptions.last) {
+					collection.push(item);
+				} else {
+					collection.splice(index, 0, item);
+				}
+				scope.$emit("itemad", item);
+
+				item.hidden = 0;
+				scope.$apply();
+				scope.$root.draggeditem = {};
 			}
-			scope.$emit("itemad", item);
-
-			scope.$apply();
-			scope.$root.draggeditem = {};
+		},
+		sortable: {
+			ngdragover: function (e, uiConfig, scope, ngModel, element) {
+				e.preventDefault();
+				e.stopPropagation();
+				element.css("border", "1px solid red");
+			},
+			ngdrop: function (e, uioptions, scope, placeholderitem) {
+				console.log("srotable drop[ ")
+			}
 		}
 	},
 	draggablehtml: {
 		pageslist: {
-			ngstart: function (e, uioptions, scope, draggedItem) {
-				scope.$root.draggeditem = draggedItem;
-				console.log("pageslist");
+			ngstart: function (e, uiConfig, scope, ngModel, element) {
+				scope.$root.dragging = true;
+				scope.$apply();
+				console.log("pageslist drag start");
 			}
 		},
 		sortable: {
 			ngstart: function (e, uioptions, scope, draggedItem) {
-				scope.$root.draggeditem = draggedItem;
-				console.log("sortable");
+				//				draggedItem.hidden = 1;
+				//				scope.$apply();
+
+				//				scope.$parent.$collection.pop();
+				console.log("sortable drag start");
 			}
 		}
 	},
@@ -54,20 +71,20 @@ module.value('ui.config', {
 module.config(['$routeProvider', '$provide', function ($routeProvider, $provide) {
 	$provide.factory('appSettings', function () {
 		var e = angular.element(".gridsmodule"),
-		    settings = {
-			    Name: e.data("application-name"),
-			    Id: e.data("application-id"),
-			    Culture: e.data("application-culture")
-		    };
+			settings = {
+				Name: e.data("application-name"),
+				Id: e.data("application-id"),
+				Culture: e.data("application-culture")
+			};
 		return settings;
 	});
 	$provide.constant();
 	$routeProvider
-		.when('/gridpages', { controller: GridListController, templateUrl: 'template/gridpages' })
-		.when('/menus', { controller: menusCtrl, templateUrl: 'template/menus' })
-		.when('/menu/:Id', { controller: menuCtrl, templateUrl: 'template/menu' })
-		.when('/gridpage/:Id', { controller: GridPageCtrl, templateUrl: 'template/gridpage' })
-		.otherwise({ redirectTo: '/gridpages' });
+		.when('/gridpages', {controller: GridListController, templateUrl: 'template/gridpages'})
+		.when('/menus', {controller: menusCtrl, templateUrl: 'template/menus'})
+		.when('/menu/:Id', {controller: menuCtrl, templateUrl: 'template/menu'})
+		.when('/gridpage/:Id', {controller: GridPageCtrl, templateUrl: 'template/gridpage'})
+		.otherwise({redirectTo: '/gridpages'});
 }]);
 
 module.controller("cultureCtrl", function ($scope, appSettings, GridApi) {
@@ -90,70 +107,70 @@ module.controller("cultureCtrl", function ($scope, appSettings, GridApi) {
 module.directive("gridelement", function ($compile, GridApi, appSettings, gridtemplate) {
 
 	function _newitem(line) {
-		var newitem = { Id: 0, Width: 12, Type: "text", Line: line, Edit: 0 };
+		var newitem = {Id: 0, Width: 12, Type: "text", Line: line, Edit: 0};
 		return newitem;
 	}
 
 	var directiveDefinitionObject,
-	    gridElementCtrl = function ($scope) {
-		    $scope.add = function (item) {
-			    GridApi.AddGridElement({
-					    applicationId: appSettings.Id,
-					    data: item,
-					    gridId: $scope.grid.Id
-				    }, function (data) {
-					    if (data.Line >= $scope.grid.Lines.length - 1) {
-						    var newitem = _newitem($scope.grid.Lines.length);
-						    $scope.grid.Lines.push([newitem]);
-					    }
-					    $scope.grid.Lines[data.Line][data.Position] = data;
-					    //TODO: nevyvola se broadcast
-					    $scope.edit(data);
-				    });
-		    };
+		gridElementCtrl = function ($scope) {
+			$scope.add = function (item) {
+				GridApi.AddGridElement({
+						applicationId: appSettings.Id,
+						data: item,
+						gridId: $scope.grid.Id
+					}, function (data) {
+						if (data.Line >= $scope.grid.Lines.length - 1) {
+							var newitem = _newitem($scope.grid.Lines.length);
+							$scope.grid.Lines.push([newitem]);
+						}
+						$scope.grid.Lines[data.Line][data.Position] = data;
+						//TODO: nevyvola se broadcast
+						$scope.edit(data);
+					});
+			};
 
-		    $scope.remove = function (item) {
-			    var line = $scope.$parent.$parent.line;
+			$scope.remove = function (item) {
+				var line = $scope.$parent.$parent.line;
 
-			    GridApi.DeleteGridElement({ applicationId: appSettings.Id, data: item, gridId: $scope.grid.Id },
-				    function () {
-					    item.Id = 0;
-					    item.Edit = 0;
-					    item.Content = "";
-					    //refresh - preopocitani poradi radku
-					    if (line.length === 1) {
-						    $scope.$emit("refreshgrid");
-					    }
-				    });
-		    };
+				GridApi.DeleteGridElement({applicationId: appSettings.Id, data: item, gridId: $scope.grid.Id},
+					function () {
+						item.Id = 0;
+						item.Edit = 0;
+						item.Content = "";
+						//refresh - preopocitani poradi radku
+						if (line.length === 1) {
+							$scope.$emit("refreshgrid");
+						}
+					});
+			};
 
-		    $scope.edit = function (item) {
-			    $scope.$broadcast("gridelement-edit");
-			    item.Edit = 1;
-		    };
+			$scope.edit = function (item) {
+				$scope.$broadcast("gridelement-edit");
+				item.Edit = 1;
+			};
 
-		    $scope.save = function (item) {
-			    var copy = jQuery.extend(true, { }, item);
+			$scope.save = function (item) {
+				var copy = jQuery.extend(true, {}, item);
 
-			    if (angular.isObject(copy.Content)) {
-				    copy.Content = JSON.stringify(copy.Content);
-			    }
+				if (angular.isObject(copy.Content)) {
+					copy.Content = JSON.stringify(copy.Content);
+				}
 
-			    GridApi.UpdateGridElement({ applicationId: appSettings.Id, data: copy },
-				    function () {
-					    item.Edit = 0;
-				    });
-		    };
+				GridApi.UpdateGridElement({applicationId: appSettings.Id, data: copy},
+					function () {
+						item.Edit = 0;
+					});
+			};
 
-	    };
+		};
 
 	directiveDefinitionObject = {
-		scope: { grid: "=", gridelement: "=" },
+		scope: {grid: "=", gridelement: "="},
 		controller: gridElementCtrl,
 		link: function (scope, iElement, tAttrs, controller) {
-			scope.gui = { edit: 0 };
+			scope.gui = {edit: 0};
 			var sablona = gridtemplate(scope.gridelement.Type),
-			    compiled = $compile(sablona)(scope);
+				compiled = $compile(sablona)(scope);
 			iElement.html(compiled);
 		}
 	};
@@ -169,12 +186,12 @@ module.directive("menuitem", function ($compile, GridApi, appSettings, menuItemT
 	};
 
 	return {
-		scope: { grid: "=", menuitem: "=" },
+		scope: {grid: "=", menuitem: "="},
 		controller: menuItemCtrl,
 		link: function (scope, iElement, tAttrs, controller) {
-			scope.gui = { edit: 0 };
+			scope.gui = {edit: 0};
 			var sablona = menuItemTemplate(scope.menuitem.Type),
-			    compiled = $compile(sablona)(scope);
+				compiled = $compile(sablona)(scope);
 			iElement.html(compiled);
 		}
 	};
--- cms/Scripts/angular-ui-ext.js
+++ cms/Scripts/angular-ui-ext.js
@@ -1,52 +1,83 @@
 angular.module('ui.directives').directive('uiDraggablehtml', [
-		'ui.config', function (uiConfig) {
-			var options;
-			options = {};
-			if (uiConfig.draggablehtml !== null) {
-				angular.extend(options, uiConfig.draggablehtml);
-			}
-			return {
-				require: '?ngModel',
-				link: function (scope, element, attrs, ngModel) {
-					var opts,
-					    namespace;
-					namespace = attrs.uiDraggablehtml;
-					opts = angular.extend({}, options, scope.$eval(attrs.uiOptions));
-					$(element).on("dragstart", function (e) {
-						if (typeof (opts[namespace].ngstart) === "function") {
-							var modelvalues = ngModel ? ngModel.$modelValue : null;
-							opts[namespace].ngstart(e, options, scope, modelvalues);
-						}
-					});
-				}
-			};
+	'ui.config', function (uiConfig) {
+		var options;
+		options = {};
+		if (uiConfig.draggablehtml !== null) {
+			angular.extend(options, uiConfig.draggablehtml);
 		}
-	]);
-		angular.module('ui.directives').directive('uiDropableHtml', [
-		'ui.config', function (uiConfig) {
-			var options;
-			options = {};
-			if (uiConfig.dropablehtml !== null) {
-				angular.extend(options, uiConfig.dropablehtml);
+		return {
+			require: '?ngModel',
+			link: function (scope, element, attrs, ngModel) {
+				var opts,
+					namespace;
+				namespace = attrs.uiDraggablehtml;
+				opts = angular.extend({}, options, scope.$eval(attrs.uiOptions));
+				
+				$(element).on("dragstart", function (e) {
+					var modelvalues = ngModel ? ngModel.$modelValue : null;
+					
+					scope.$root.draggeditem = modelvalues;
+					scope.$root.draggeditem.namespace = namespace;
+					
+					console.log(scope.$root)
+					if (typeof (opts[namespace].ngstart) === "function") {
+						opts[namespace].ngstart(e, options, scope, modelvalues);
+					}
+				});
+				$(element).on("dragend", function (e) {
+					var modelvalues = ngModel ? ngModel.$modelValue : null;
+				
+					if (typeof (opts[namespace].ngdragend) === "function") {
+						opts[namespace].ngdragend(e, options, scope, modelvalues, element);
+					}
+				});
 			}
-			return {
-				require: '?ngModel',
-				link: function (scope, element, attrs, ngModel) {
-					var opts;
-					opts = angular.extend({}, options, scope.$eval(attrs.uiOptions));
-					$(element).on("dragover", function (e) {
-						e.stopPropagation();
-						e.preventDefault();
-						//opts.ngstart(e, scope);
-					});
-					$(element).on("drop", function (e) {
+		};
+	}
+]);
+	angular.module('ui.directives').directive('uiDropableHtml', [
+	'ui.config', function (uiConfig) {
+		var options;
+		options = {};
+		if (uiConfig.dropablehtml !== null) {
+			angular.extend(options, uiConfig.dropablehtml);
+		}
 
-						if (typeof opts.ngdrop === "function") {
-							var modelvalues = ngModel ? ngModel.$modelValue : null;
-							return opts.ngdrop(e, opts, scope, modelvalues);
-						}
-					});
-				}
-			};
+		function getNamespace(scope, attrs) {
+			var validNamespaceIndex, validNamespace;
+			var namespaceArray = attrs.uiDropableHtml.split(",");
+
+			if (scope.$root.draggeditem) {
+				validNamespaceIndex = namespaceArray.indexOf(scope.$root.draggeditem.namespace);
+				validNamespace = namespaceArray[validNamespaceIndex];
+			}
+			return validNamespace;
 		}
-	]);
+
+		return {
+			require: '?ngModel',
+			link: function (scope, element, attrs, ngModel) {
+				var opts;
+				opts = angular.extend({}, options, scope.$eval(attrs.uiOptions));
+
+				$(element).on("dragover", function (e) {
+					var modelvalues = ngModel ? ngModel.$modelValue : null;
+
+					var namespace = getNamespace(scope, attrs);
+					if (namespace && typeof opts[namespace].ngdragover === "function") {
+						return opts[namespace].ngdragover(e, opts, scope, modelvalues, element);
+					}
+				});
+				
+				$(element).on("drop", function (e) {
+					var modelvalues = ngModel ? ngModel.$modelValue : null;
+
+					var namespace = getNamespace(scope, attrs);
+					if (namespace && typeof opts[namespace].ngdrop === "function") {
+						return opts[namespace].ngdrop(e, opts, scope, modelvalues);
+					}
+				});
+			}
+		};
+	}
+]);
