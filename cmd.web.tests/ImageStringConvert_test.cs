d7637c5dd5f7d83aaad422e67df68f5eaa7e9945
v.talas <v.talas@gmc.net>
2013-07-12 15:52:31 +0200

    image size convertor

---
+++ cmd.web.tests/ImageStringConvert_test.cs
@@ -0,0 +1,20 @@
+﻿using cms.Controllers.Api;
+using NUnit.Framework;
+
+namespace cms.web.tests
+{
+	[TestFixture]
+	public class ImageStringConvert_test
+	{
+		[Test]
+		public void test()
+		{
+			var x = ImageStringConvert.Create(1900, 1200, ImageSizeType.MaxWidth, 100);
+
+
+			Assert.AreEqual();
+
+		}
+
+	}
+}
---
+++ cms/Controllers/Api/ImageStringConvert.cs
@@ -0,0 +1,67 @@
+﻿using System.Web.UI.WebControls;
+
+namespace cms.Controllers.Api
+{
+	public enum ImageSizeType
+	{
+		MaxWidth,
+		MaxHeight,
+		MaxSize
+	}
+	
+	public class ImageStringConvert
+	{
+		private readonly int _width;
+		private readonly int _height;
+		private readonly ImageSizeType _type;
+		private readonly int _value;
+		private readonly bool _square;
+
+		private double Ratio { get; set; }
+		private Orientation Orientation { get; set; }
+
+		private ImageStringConvert(int width, int height, ImageSizeType type, int value, bool square)
+		{
+			_width = width;
+			_height = height;
+			_type = type;
+			_value = value;
+			_square = square;
+
+			Orientation = _width > _height ? Orientation.Horizontal : Orientation.Vertical;
+			Ratio = (double)_width / _height;
+		}
+
+		public static ImageStringConvert Create(int width, int height, ImageSizeType type, int value, bool isSquare = false)
+		{
+			var instance =  new ImageStringConvert(width, height, type,value, isSquare);
+			return instance;
+		}
+
+
+		public int Width
+		{
+			get
+			{
+				return 0;
+			}
+		}
+		
+		public int Height
+		{
+			get
+			{
+				return 0;
+			}
+		}
+
+		public string DimensionString
+		{
+			get
+			{
+				return 
+			}
+		}
+
+	}
+}
